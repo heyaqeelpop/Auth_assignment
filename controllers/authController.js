71a9e3cbadc6b8733e2800fb8df0c3078f0a13fc
heyaqeelpop <compaqeel@gmail.com>
2026-09-23 22:44:34 +0530

    stage 1: signup and login routes working

---
+++ controllers/authController.js
@@ -0,0 +1,57 @@
+const {createClient} = require("@supabase/supabase-js");
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+
+const signup = async (req, res) => {
+  const { email, password } = req.body;
+  if(!email || !password) {
+    return res.status(400).json({
+        error : "email and password are required"
+    })
+  }
+  const {data,error} = await supabase.auth.signUp({
+    email,
+    password
+  });
+
+  if(error){
+    return res.status(400).json({
+      error : error.message
+    });
+  }
+
+  return res.status(201).json({
+    user : data.user
+  })
+};
+
+
+const login = async(req,res)=>{
+  const {email,password} = req.body;
+  if(!email || !password){
+    return res.status(400).json({
+      error : "Email and password are required"
+    })
+  }
+
+  const {data,error}= await supabase.auth.signInWithPassword({
+    email,
+    password
+  })
+
+  if(error){
+    return res.status(401).json({
+      error : "inavlid login credentials"
+    })
+  }
+
+  return res.status(200).json({
+    access_token: data.session.access_token,
+    refresh_token: data.session.refresh_token
+  });
+};
+
+
+module.exports = {
+  signup,
+  login
+};
--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 require("dotenv").config();
 const express = require("express");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
+app.use(express.json());
+
+app.use("/auth",authRoutes);
 
 app.get("/", (req, res) => {
   res.send("Server is running");
